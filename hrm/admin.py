d4bd35598b3dd89b09f2c9e221086a999149bb9a
Roberto Zaniol <info@rzweb.it>
2024-03-25 22:36:09 +0100

    set admin site for hrm

--- hrm/admin.py
+++ hrm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Level, Task, Worker
+
 # Register your models here.
+admin.site.register(Level)
+admin.site.register(Task)
+admin.site.register(Worker)
