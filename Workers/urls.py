15347f0ef6a0cac2560d5250424a13b81c49bfd5
Roberto Zaniol <info@rzweb.it>
2024-03-30 11:45:22 +0100

    added home page project

--- Workers/urls.py
+++ Workers/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('hrm/', include('hrm.urls')),
     path('job/', include('job.urls')),
     path('admin/', admin.site.urls),
