b327bfaf1312f12decda76819c5e76ac81b266f1
Roberto Zaniol <info@rzweb.it>
2024-04-01 10:41:22 +0200

    first home page, set static files and template files

--- Workers/views.py
+++ Workers/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from hrm.models import Worker
+from job.models import Contract, SubContract
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Questa è la prima pagina del progetto Workers')
+    num_workers = Worker.objects.all().count()
+    num_contracts = Contract.objects.all().count()
+
+    context = {
+        'num_workers': num_workers,
+        'num_contracts': num_contracts,
+    }
+    return render(request, 'index.html', context=context)
--- hrm/admin.py
+++ hrm/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Level, Task, Worker
+from .models import Level, Task, Worker, Career, Assignments, TimeRecording
 
 # Register your models here.
 admin.site.register(Level)
 admin.site.register(Task)
 admin.site.register(Worker)
+admin.site.register(Career)
+admin.site.register(Assignments)
+admin.site.register(TimeRecording)
